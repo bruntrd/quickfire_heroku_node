123fefea23846770eb93a489ea0e96ef35ffba87
bruntrd <bruntrd@gmail.com>
2015-07-29 11:47:18 -0500

    adds file server

--- server/app.js
+++ server/app.js
@@ -5,8 +5,8 @@ var path = require('path');
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req,res){
-    res.send("hello...");
-});
+    res.sendFile(path.join(__dirname, "./public/views/index.html"));
+    });
 
 app.listen(app.get('port'), function(){
     console.log("Listening on Port:" + app.get('port'));
